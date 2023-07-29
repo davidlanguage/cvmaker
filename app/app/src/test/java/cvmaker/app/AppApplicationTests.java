eed483333aa32b8e40c5b8e574e66530ed48d650
david <davidparadela.r@gmail.com>
2023-07-29 17:12:21 +0200

    first commit

---
+++ app/app/src/test/java/cvmaker/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package cvmaker.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
