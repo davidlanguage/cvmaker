8a8ebbd4c8fb86c0b48594db936b805b8891c21f
david <davidparadela.r@gmail.com>
2023-08-27 15:44:17 +0200

    command line runner and bean test case

--- back/app/src/main/java/cvmaker/app/AppApplication.java
+++ back/app/src/main/java/cvmaker/app/AppApplication.java
@@ -1,13 +1,22 @@
 package cvmaker.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(){
+		return args -> {
+			System.out.println("Thank you for using my app");
+		};
 	}
 
 }
