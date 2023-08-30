76b64062ad5b8f346f4c4484e2d68f6b9dce9487
david <davidparadela.r@gmail.com>
2023-08-30 12:07:27 +0200

    useless class eliminated

--- back/app/src/main/java/cvmaker/app/multilanguage/MessageSourceConfiguration.java
+++ back/app/src/main/java/cvmaker/app/multilanguage/MessageSourceConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
 @Configuration
-public class MessageConfiguration {
+public class MessageSourceConfiguration {
 
     @Bean
     public MessageSource messageSource(){
