89fd47f347241b8c530b5fa1bd958561fd8a8d6d
david <davidparadela.r@gmail.com>
2023-09-04 16:12:30 +0200

    components create and model folder

--- front/cvmaker-web/src/app/app.module.ts
+++ front/cvmaker-web/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './components/header/app-header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CenterComponent } from './components/center/center.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppHeaderComponent
+    AppHeaderComponent,
+    FooterComponent,
+    CenterComponent,
+    RegistrationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
