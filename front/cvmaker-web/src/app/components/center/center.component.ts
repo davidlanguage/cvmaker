45d502f32a7ea147f9cb1eb421a410e9b0bd38b2
david <davidparadela.r@gmail.com>
2023-09-04 16:50:10 +0200

    add mid component

--- front/cvmaker-web/src/app/components/center/center.component.ts
+++ front/cvmaker-web/src/app/components/center/center.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./center.component.scss']
 })
 export class CenterComponent {
+  title = "Title not provided";
+  description = `
+  Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+  ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient
+  montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium
+   quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec,
+   ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+  ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient
+  montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium
+   quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec,
+  Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+  ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient
+  montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium
+   quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec,
+   ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+  ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient
+  montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium
+   quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec,
+   `
 
+   //TODO: init() the title and description
 }
